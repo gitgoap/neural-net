6d1e8e2cb134dbb3fe2521f8e7c6e74ca6cde579
Aman Prakash <futureofaman@gmail.com>
2025-04-17 19:39:59 +0530

    lesson 6 update

--- p6_softmax.py
+++ p6_softmax.py
@@ -1,17 +1,53 @@
-#input --> Exponential --> Normalize --> Output
+#input --> Exponential --> Softmax Normalization --> Output
 
-import math
-E=math.e # gived the value of euler constant i.e. 2.71828
-X=[2.3,4.5,-0.9]
+#Every thing from scratch
+'''
+E=2.71828 # import math E=math.e # gives the value of euler constant i.e. 2.71828
+X=[4.8,1.21,2.385]
 exp_value=[]
 normalized_value=[]
+sum=0
 for value in X:
     exp_value.append(E**value)
-    sum=0+value
+    sum=sum+E**value
 for final in exp_value:
     normalized_value.append(final/sum)
 
 print(normalized_value)    
+print(sum)
+'''
 
 
 
+#Using Numpy lib
+'''
+import numpy as np
+X=[4.8,1.21,2.385]
+exp_value=np.exp(X)
+normalized_value=exp_value/np.sum(exp_value)
+print(normalized_value)
+
+'''
+
+#For Batches
+'''
+import numpy as np
+X=[[2,3,6],
+   [4,1,2],
+   [-4,1,5]]
+exp_value=np.exp(X)
+normalized_value=exp_value/ np.sum(exp_value, axis=1, keepdims=True)
+print(normalized_value)
+
+#axis=0 : column; axis=1 : rows
+'''
+
+#overflow prevention
+'''
+ error like: e^1000 gives Runtime warning 
+ prevention: final values (v) = ui- max (ui)
+             e.g. [2,3,1] after prevention [2-3,3-3,1-3] = [-1,0,-2] 
+ prevention doesn't effect final normalized value.             
+'''
+
+#full code 
