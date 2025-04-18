833b300f68f84a11387184ad305e4edcd541461c
Aman Prakash <futureofaman@gmail.com>
2025-04-18 18:34:40 +0530

    lesson 7 complete

---
+++ p7_cross_entropy.py
@@ -0,0 +1,14 @@
+# cross-entropy for classification problem
+import numpy as np
+predicted_output=[0.7, 0.1, 0.2]  # These output came from softmax function
+target_output=   [1, 0, 0]        #0.7 should be 1 others should be 0, like a one-hot vector
+
+cross_entropy_loss = -(np.log(predicted_output[0])*target_output[0] + 
+                       np.log(predicted_output[1])*target_output[1] +
+                       np.log(predicted_output[2])*target_output[2])
+
+print(cross_entropy_loss)
+
+# verification
+# import math
+# print(math.e **- 0.35667494393873245)
